afe0d552c6512cca5cdd58277ae76d9dcbbd79d6
Irena <irenva101@gmail.com>
2024-08-06 09:47:30 +0200

    navigating via url to new task of each user

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input } from '@angular/core';
+import { UsersService } from '../users.service';
+import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
+  imports: [RouterOutlet, RouterLink] 
 })
-export class UserTasksComponent {}
+export class UserTasksComponent {
+  //userId= input.required<string>();
+  private usersService = inject(UsersService);
+  private activedRoute = inject(ActivatedRoute);
+  private destoryRef = inject(DestroyRef);
+  userName='';
+
+  //userName = computed(()=> this.usersService.users.find(u => u.id === this.userId())?.name);
+
+  ngOnInit(): void {
+    console.log(this.activedRoute);
+    const subscription = this.activedRoute.paramMap.subscribe({
+      next: (paramMap) => {
+        this.userName =
+          this.usersService.users.find((u) => u.id === paramMap.get('userId'))
+            ?.name || '';
+      },
+    });
+
+    this.destoryRef.onDestroy(()=> subscription.unsubscribe());
+  }
+}
