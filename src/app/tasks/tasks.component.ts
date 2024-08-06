e9a4b5e9c5df10caf23f7eb520ebd69688bd008a
Irena <irenva101@gmail.com>
2024-08-06 13:33:14 +0200

    implemented asc, desc sorting of the tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,9 +1,17 @@
-import { Component, computed, inject, input } from '@angular/core';
+import {
+  Component,
+  computed,
+  DestroyRef,
+  inject,
+  input,
+  OnInit,
+  signal,
+} from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
 import { TasksService } from './tasks.service';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tasks',
@@ -12,8 +20,31 @@ import { RouterLink } from '@angular/router';
   styleUrl: './tasks.component.css',
   imports: [TaskComponent, RouterLink],
 })
-export class TasksComponent {
-  userId= input.required<string>();
-  private tasksService= inject(TasksService);
-  userTasks = computed(()=> this.tasksService.allTasks().filter((task)=> task.userId === this.userId()));
+export class TasksComponent implements OnInit {
+  userId = input.required<string>();
+  //order=input<'asc' | 'desc'>();
+  order = signal<'asc' | 'desc'>('desc');
+  private tasksService = inject(TasksService);
+  userTasks = computed(() =>
+    this.tasksService
+      .allTasks()
+      .filter((task) => task.userId === this.userId())
+      .sort((a, b) => {
+        if (this.order() === 'desc') {
+          return a.id > b.id ? -1 : 1;
+        } else {
+          return a.id > b.id ? 1 : -1;
+        }
+      })
+  );
+  private activatedRoute = inject(ActivatedRoute);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit() {
+    const subscription = this.activatedRoute.queryParams.subscribe({
+      next: (params) => this.order.set(params['order']),
+    });
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 }
